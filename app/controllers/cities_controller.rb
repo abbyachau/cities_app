38f0099f1bd8a2a2a085b4c1b44965c8b04d7106
abbyachau <abbyachau@gmail.com>
2016-06-22 23:39:36 -0700

    working home page and main nav

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,29 @@
 class CitiesController < ApplicationController
-  def cities
-  end
+  def index
+    @cities = City.all
+end
+
+  def new
+    @cities = City.new
+end
+
+  def create
+    @city = City.new(city_params)
+    if @city.save
+      redirect_to cities_path
+    else
+      render "new"
+    end
+end
+
+private
+def get_city
+   City.find(params[:id])
+end
+
+def city_params
+  params.require(:city).permit(:name, :image_url, :description)
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-
+resources :cities
 #Custom routes
-get 'home', to: "static_pages#home", as: 'home'
+get 'about', to: "static_pages#about", as: 'about'
+get 'sign_in', to: "static_pages#sign_in", as: 'sign_in'
 get 'test', to: "static_pages#test", as: 'test'
 
 #sets home_page
